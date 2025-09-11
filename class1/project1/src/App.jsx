26a061b0bb735e6f463598c3e2a5998f09194c57
Rohit-0-0 <rohitpatel90878@gmail.com>
2025-09-11 15:49:46 +0530

    project 2

--- class1/project1/src/App.jsx
+++ class1/project1/src/App.jsx
@@ -5,29 +5,25 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+    let [name,setName] = useState("Rohit")
+    let [age, setAge] = useState(21)
+    let [gender, setGender] = useState("Male")
+    let [address, setAddress] = useState("Noida")
+  function op (){
+    setName("hello world")
+    setAge(22)
+    setAddress("Delhi")
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div className='card'>
+        <p>Name : {name}</p>
+        <p>Age : {age}</p>
+        <p>Gender : {gender}</p>
+        <p>Address : {address}</p>
+        </div>
+        <button onClick={op}> Change Name</button>
     </>
   )
 }
---
+++ class2/project2/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+      <div>
+        <p> This is an app </p>
+      </div>
+  )
+}
+
+export default App
